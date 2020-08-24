a7e6e462b7b3bdd3af98e82579bb7f3448a66951
Bahadır <dogrubahadir@gmail.com>
2020-08-24 13:29:14 +0300

    Update debian9-php7.4-nginx-mariadb-ajenti.sh

--- debian9-php7.4-nginx-mariadb-ajenti.sh
+++ debian9-php7.4-nginx-mariadb-ajenti.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # For Debian 9 Stretch
@@ -8,7 +7,7 @@
 # Swap sources from https://www.digitalocean.com/community/tutorials/how-to-add-swap-on-ubuntu-14-04
 # Multiline creating texts https://stackoverflow.com/questions/40562595/creating-an-output-file-with-multi-line-script-using-echo-linux
 # Nginx Fail2ban configuration https://www.digitalocean.com/community/tutorials/how-to-protect-an-nginx-server-with-fail2ban-on-ubuntu-14-04
-# Php7.2 explanation from https://tecadmin.net/install-php-debian-9-stretch/
+# Php7.4 explanation from https://tecadmin.net/install-php-debian-9-stretch/
 
 # Bismillah
 clear;
@@ -19,6 +18,11 @@ sleep 3;
 clear;
 echo "---*** We don't need any mail packages like exim4 ***---";
 apt-get -y remove --purge exim4*; 
+apt-get -y remove --purge postfix*; 
+# Cleaning
+apt-get -y autoremove;
+apt-get -y autoclean;
+
 
 # I change ssh port to 22222. Do not use default never.
 clear;
@@ -67,14 +71,15 @@ apt-key add nginx_signing.key;
 echo deb http://nginx.org/packages/debian/ stretch nginx >> /etc/apt/sources.list;
 echo deb-src http://nginx.org/packages/debian/ stretch nginx >> /etc/apt/sources.list;
 
-# Add Mysql Repository
-# Get latest link from https://dev.mysql.com/downloads/repo/apt/
+# Add MariaDB Repository
+# Get latest link from https://downloads.mariadb.org/mariadb/repositories/#mirror=digitalocean-ams
 clear;
-echo "---*** Adding Mysql repository ***---";
+echo "---*** Adding  MariaDB repository ***---";
 sleep 3;
-wget https://dev.mysql.com/get/mysql-apt-config_0.8.10-1_all.deb
-dpkg -i mysql-apt-config_0.8.10-1_all.deb
-rm mysql-apt-config_0.8.10-1_all.deb
+sudo apt-get install software-properties-common dirmngr
+sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://ams2.mirrors.digitalocean.com/mariadb/repo/10.5/debian stretch main'
+
 
 # Cleaning and updating server to be ready.
 apt-get -y autoremove;
@@ -86,10 +91,11 @@ apt-get -y upgrade;
 clear;
 echo "---*** Start Installation ***---";
 sleep 3;
-apt-get -y install nginx unzip zip fail2ban php7.2-fpm php7.2-mbstring php7.2-curl php7.2-gd php7.2-mysql php7.2-soap php7.2-xml php7.2-json php7.2-zip php7.2-cli php7.2-opcache php7.2-common mysql-server;
+apt-get -y install nginx unzip zip fail2ban php7.4 php7.4-fpm php7.4-mbstring php7.4-curl php7.4-gd php7.4-mysql php7.4-soap php7.4-xml php7.4-json php7.4-zip php7.4-cli php7.4-opcache php7.4-common php7.4-readline mariadb-server;
 # optional? redis-server memcached php-redis php-memcached
+
 wget -O- https://raw.github.com/ajenti/ajenti/1.x/scripts/install-debian.sh | sh;
-apt-get -y install ajenti-v ajenti-v-mysql ajenti-v-php7.2-fpm ajenti-v-ftp-pureftpd ajenti-v-nginx;
+apt-get -y install ajenti-v ajenti-v-mysql ajenti-v-php7.4-fpm ajenti-v-ftp-pureftpd ajenti-v-nginx;
 
 # Install Composer
 curl -sS https://getcomposer.org/installer | php;
@@ -109,16 +115,16 @@ echo "---*** Configure Mysql ***---";
 sleep 3;
 mysql_secure_installation
 
-# In Ajenti python files there is a forgotten bug. This code blocks correct that.
-sed -i 's/("Access-Control-Max-Age", 3600)/("Access-Control-Max-Age", "3600")/g' /usr/share/pyshared/socketio/transports.py;
-sed -i 's/("Access-Control-Max-Age", 3600)/("Access-Control-Max-Age", "3600")/g' /usr/share/pyshared/socketio/handler.py;
+# In Ajenti python files there is a forgotten bug. This code blocks correct that. and double it.
+sed -i 's/("Access-Control-Max-Age", 3600)/("Access-Control-Max-Age", "7200")/g' /usr/share/pyshared/socketio/transports.py;
+sed -i 's/("Access-Control-Max-Age", 3600)/("Access-Control-Max-Age", "7200")/g' /usr/share/pyshared/socketio/handler.py;
 clear;
 echo "Server-Automate Installation is ascomplished !!!";
 echo " ";
 echo "Ajenti Panel: https://ipadress:8000";
 echo "SSH Port is now 22222";
 echo "2GB Swap ram is configured";
-echo "Php7.2 Nginx Mysql8 is installed and can be configured in Ajenti Panel";
+echo "Php7.4 Nginx MariaDB15 is installed and can be configured in Ajenti Panel";
 echo "Server will be restart in 30 seconds";
 sleep 30;
 reboot
